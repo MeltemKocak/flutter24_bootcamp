47caa040a58968dea4ffb882de3560668a9f07d8
olgnbrn <olgnbrn@gmail.com>
2024-07-11 13:17:42 +0300

    Sign up and login pages added
    Sign up and login connected to Firebase
    User data viewed through firebase

--- planova/lib/main.dart
+++ planova/lib/main.dart
@@ -1,45 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'pages/login_screen.dart'; // Giriş ekranını içe aktarın
+import 'package:planova/firebase_options.dart';
+import 'package:planova/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:planova/pages/sign_up_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-
-  print("API Key: ${dotenv.env['FIREBASE_API_KEY']}");
-  print("App ID Android: ${dotenv.env['FIREBASE_APP_ID_ANDROID']}");
-  print("App ID iOS: ${dotenv.env['FIREBASE_APP_ID_IOS']}");
-
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    print("Firebase initialized successfully");
-  } catch (e) {
-    print("Error initializing Firebase: $e");
-  }
-
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    print("Building MyApp");
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(), // Giriş ekranını ana ekran olarak ayarlayın
+      debugShowCheckedModeBanner: false,
+      home: Login()
     );
   }
 }
